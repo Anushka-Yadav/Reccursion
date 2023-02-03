1511e7fae0123c95b9d66b03618542d7a7e60cc1
Anushka Yadav <113161007+Anushka-Yadav@users.noreply.github.com>
2023-02-03 23:43:17 +0530

    first commit
    power

---
+++ Power.java
@@ -0,0 +1,18 @@
+public class Power {
+    public static int power(int x,int n) {
+       if(x==0){
+        return 0;
+       }
+       if(x==1 || n==0){
+        return 1;
+       }
+       int g = power(x, n-1);
+       int pow = x * g;
+       return pow;
+    
+    }
+    public static void main(String[] args) {
+        int k = power(5, 2);
+        System.out.println(k);
+    }
+}
